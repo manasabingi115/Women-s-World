c37061424ba6f71c62c4e8fde3f5aec265582f6a
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-08-08 21:59:09 +0530

    added code.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
 import Header from "./header-folder/header";
+import HoveredContent from "./hovered-folder/hovered-content";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <div className="body-content">
+        <HoveredContent />
+      </div>
     </div>
   );
 }
---
+++ src/hovered-folder/hovered-content.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HoveredContent() {
+  return (
+    <div className="hovered-content">
+      <h1>Hovered Content</h1>
+    </div>
+  );
+}
